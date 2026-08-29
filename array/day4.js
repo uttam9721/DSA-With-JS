440093e42f35e76bde84f5d4c2318bdd6aad5ab2
uttam9721 <uttammaurya377@gmail.com>
2026-08-29 21:50:23 +0530

    day4

---
+++ array/day4.js
@@ -0,0 +1,11 @@
+// function List({ items }) {
+//   return (
+//     <ul>
+//       {items.map((item, index) => (
+//         <li key={index}>
+//           <input type="text" defaultValue={item.name} />
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
