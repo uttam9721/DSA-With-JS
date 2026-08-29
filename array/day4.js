e71947a85326b0d8bf252edfa492a83e005725f5
uttam9721 <uttammaurya377@gmail.com>
2026-08-29 22:07:20 +0530

    day4

--- array/day4.js
+++ array/day4.js
@@ -9,3 +9,22 @@
 //     </ul>
 //   );
 // }
+// let num=Number(prompt("Enter the number of elements in the array"));
+
+// WAP to find the sum of first 100 natural numbers.
+ 
+// let num =100;
+// let sum = 0;
+// for(let i=1;i<=num;i++){
+//     sum=sum+i;
+// }
+// console.log("Sum of first " + num + " natural numbers is: " + sum);
+
+
+// WAP to find factorial of a number.
+let num=5;
+let factorial=1;
+for(let i=1;i<=num;i++){
+    factorial=factorial*i;
+}
+console.log("Factorial of " + num + " is: " + factorial);
