00b0de444893846d9c50154cec5b9a0bf678e996
uttam9721 <uttammaurya377@gmail.com>
2026-09-04 23:57:33 +0530

    day5

--- array/day5.js
+++ array/day5.js
@@ -1,6 +1,17 @@
-let arr = [10,20,30,40,50];
-let sum=0;
+// let arr = [10,20,30,40,50];
+// let sum=0;
+// for(let i=0;i<arr.length;i++){
+//     sum=sum+arr[i];
+// }
+// console.log("Sum of array elements is: " + sum);
+
+
+let arr=[51,42,89,6,5,2,1];
+let max=arr[0];
 for(let i=0;i<arr.length;i++){
-    sum=sum+arr[i];
+    if(arr[i]>max){
+        max=arr[i];
+    }
+
 }
-console.log("Sum of array elements is: " + sum);
+console.log("Maximum element in the array is: " + max);
