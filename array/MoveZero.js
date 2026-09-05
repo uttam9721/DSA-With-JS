eb3ab60a368fecaa143675270aca0baf63faac8b
uttam9721 <uttammaurya377@gmail.com>
2026-09-05 22:52:30 +0530

    movezero

---
+++ array/MoveZero.js
@@ -0,0 +1,13 @@
+let arr=[0,0,1,1,0,1,0,1,0,0,1,1,0,1,0,1,0,0,1,1];
+let i=0;
+let j=0;
+while(i<arr.length){
+    if(arr[i]==0){
+        let temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+        j++;
+    }
+    i++;
+}
+console.log(arr);
