e3b6d6bb9064dc948552aa07679b892005ceda98
uttam9721 <uttammaurya377@gmail.com>
2026-09-07 23:34:14 +0530

    right-rotate

--- array/LeftRotate.js
+++ array/LeftRotate.js
@@ -1,7 +1,9 @@
 let arr=[1,2,3,4,5];
+for(j=0;j<2;j++){
 let copy=arr[0];
 for(let i=0;i<arr.length-1;i++){
     arr[i]=arr[i+1];
 }
 arr[arr.length-1]=copy;
+}
 console.log(arr);
---
+++ array/RightRotate.js
@@ -0,0 +1,7 @@
+let arr=[1,2,3,4,5];
+let copy=arr[arr.length-1];
+for(let i=arr.length-1;i>0;i--){
+    arr[i]=arr[i-1];
+}   
+arr[0]=copy;
+console.log(arr);
