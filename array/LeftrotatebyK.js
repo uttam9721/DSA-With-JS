4a962f964b358234179d19dc6bb43c40a9cfbca0
uttam9721 <uttammaurya377@gmail.com>
2026-09-14 23:27:41 +0530

    practice

--- array/LeftrotatebyK.js
+++ array/LeftrotatebyK.js
@@ -1,11 +1,24 @@
-let arr=[1,2,3,4,5];
-let k=2;
-for(let j=0;j<k;j++){
-let copy=arr[0];
-for(let i=0;i<k;i++){
-    arr=[i]=arr[i+1];
+// let arr=[1,2,3,4,5];
+// let k=2;
+// for(let j=0;j<k;j++){
+// let copy=arr[0];
+// for(let i=0;i<k;i++){
+//     arr=[i]=arr[i+1];
 
-}
-arr[arr.length-1]=copy;
+// }
+// arr[arr.length-1]=copy;
+// }
+// console.log(arr)
+
+
+let arr=[1,2,3,4,5];
+let i=0;
+let j=arr.length-1;
+while(i!=j){
+    let temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+    i++;
+    j--;
 }
 console.log(arr)
